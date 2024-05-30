3a00348e491827deb636e4a6439c3df1a2643318
bton2 <limabenilton@gmail.com>
2024-05-30 09:48:31 -0300

    Exercícoi 17

--- aula14ex/ex17/script.js
+++ aula14ex/ex17/script.js
@@ -1,1 +1,20 @@
-alert('Hello')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Please, enter with a number')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        // Limpa a área antes de mostra uma nova tabuada
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }       
+    
+}
